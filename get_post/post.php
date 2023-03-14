66042b4ad501b97e73d28db2f83050011f9dcf92
Nitish <nitishrajbongshi@gmail.com>
2023-03-14 23:49:48 +0530

    Add get and post method

---
+++ get_post/post.php
@@ -0,0 +1,9 @@
+<?php
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+?>
+<div>
+    <h2>Contact Us:</h2>
+    <h5>Name: <?php echo $name; ?></h5>
+    <h5>Email: <?php echo $email; ?></h5>
+</div>
